7115f38049368970e40cb87b5f43736adf4ecf52
Björn Linse <bjorn.linse@gmail.com>
2017-10-14 12:40:44 +0200

    provider sketch

---
+++ autoload/provider/clipboard.vim
@@ -0,0 +1,36 @@
+
+let g:nvimx11_path =  expand( '<sfile>:p:h:h:h' )
+lua <<EOT
+ nvimX11 = require('nvimX11')
+EOT
+
+function! provider#clipboard#Call(method, args) abort
+  if get(s:, 'here', v:false)  " Clipboard provider must not recurse. #7184
+    return 0
+  endif
+  let s:here = v:true
+  try
+    return provider#clipboard#invoke(a:method, a:args)
+  finally
+    let s:here = v:false
+  endtry
+endfunction
+
+function! provider#clipboard#invoke(method, args) abort
+  if a:method == "get"
+    let sel = a:args[0]
+    let [str, regtype] = luaeval("{nvimX11.get(_A)}", sel)
+
+    " TODO: this should be an elementary operation:
+    if type(str) == v:t_string
+      let data = split(str, "\n", v:true)
+    elseif type(str) == v:t_dict
+      if str._TYPE != v:msgpack_types['string']
+        echoerr "blargh"
+      end
+      let data = str._VAL
+    end
+
+    return [data, regtype]
+  endif
+endfunction
--- lua/nvimX11.lua
+++ lua/nvimX11.lua
@@ -1,28 +1,35 @@
 -- TODO: local-ize
-ffi = require("ffi")
+local ffi = require("ffi")
+
+local path = vim.api.nvim_get_var("nvimx11_path")
+print(path)
 
 ffi.cdef[[
 int nvimx11_test(int x);
 char* nvimx11_getsel(int name, int* type, size_t* len);
 bool nvimx11_putsel(int name, int type, const char* data, size_t len);
 ]]
-libnvimX11 = ffi.load("build/libnvimX11.so")
+libnvimX11 = ffi.load(path.."/build/libnvimX11.so")
 
 seltype = ffi.new("int[1]")
 selsize = ffi.new("size_t[1]")
 --print(libnvimX11.nvimx11_test(3))
+--
+local typenames = {[0] = "v", [1] = "V", [2] = "b"}
+local typeids= {v=0,V=1,b=2}
 
 local function get(sel)
   p = libnvimX11.nvimx11_getsel(string.byte(sel,1), seltype, selsize)
   str = ffi.string(p, selsize[0])
-  return str, seltype[0]
+  typename = typenames[seltype[0]] or ""
+  return str, typename
 end
 
-local function put(sel,data,seltype)
+local function put(sel,data,typename)
   if type(data) ~= "string" then
     error("data must be string")
   end
-  libnvimX11.nvimx11_putsel(string.byte(sel,1), seltype, data, string.len(data))
+  libnvimX11.nvimx11_putsel(string.byte(sel,1), typeids[typename], data, string.len(data))
 end
 return {get=get, put=put}
 
